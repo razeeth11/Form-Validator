42fd6cc20b4191ce0702b4d105d49be246eb678e
razeeth11 <razeeth011@gmail.com>
2023-03-14 15:32:57 +0530

    form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 
 function App() {
 
   return (
     <div className="App">
-       
+      <form action="#">
+        <TextField className='fields' id="outlined-basic" label="Username" variant="outlined" />
+        <TextField className='fields' id="outlined-basic" label="Email" variant="outlined" />
+        <TextField className='fields' id="outlined-basic" label="Password" variant="outlined" />
+
+        <Button className='fields' variant="contained">SUBMIT</Button>
+      </form>
     </div>
   )
 }
